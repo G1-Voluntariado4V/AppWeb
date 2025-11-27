5461cef4359bad2b7d5563d0fc425e0fdcae6374
Jarol Rojas <jr.rojas0327@gmail.com>
2025-11-28 00:15:56 +0100

    feat: Agregar componente de contacto con su estructura HTML y lógica asociada

--- src/app/features/landing/layout/landing-layout.ts
+++ src/app/features/landing/layout/landing-layout.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Navbar } from "../components/navbar/navbar";
 import { Inicio } from "../pages/inicio/inicio";
 import { QuienesSomos } from "../pages/quienessomos/quienessomos";
+import { LandingContacto } from "../pages/contacto/contacto";
 
 @Component({
   selector: 'app-landing-layout',
-  imports: [Navbar, Inicio, QuienesSomos],
+  imports: [Navbar, Inicio, QuienesSomos, LandingContacto],
   templateUrl: './landing-layout.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
---
+++ src/app/features/landing/pages/contacto/contacto.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-contacto',
+  imports: [],
+  templateUrl: './contacto.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LandingContacto { }
