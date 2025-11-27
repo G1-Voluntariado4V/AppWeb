cc792ea7062ae4ee42f5486b78855a08bdf1ef83
Jarol Rojas <jr.rojas0327@gmail.com>
2025-11-27 22:42:41 +0100

    feat: Agregar componente "¿Quiénes Somos?" con su estructura HTML y lógica asociada

---
+++ src/app/features/landing/pages/quienessomos/quienessomos.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'landing-quienes-somos',
+  imports: [],
+  templateUrl: './quienessomos.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class QuienesSomos { }
