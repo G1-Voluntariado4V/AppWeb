592bcf9294b51c00519799b338ceba66d5916f62
Jarol Rojas <jr.rojas0327@gmail.com>
2025-12-01 13:16:17 +0100

    feat: agregar configuración de entorno para producción y desarrollo de Firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyCnXZhruPbioenFOnWyazu9aQ6oivrNr6o",
+        authDomain: "voluntariado4v.firebaseapp.com",
+        projectId: "voluntariado4v",
+        storageBucket: "voluntariado4v.firebasestorage.app",
+        messagingSenderId: "209714054260",
+        appId: "1:209714054260:web:9a8643aa609e471062fd21",
+        measurementId: "G-3YR6XC9FRG"
+    }
+};
