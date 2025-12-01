44fdc29a078509686b0f34ad7930dc80c34a7bf9
Jarol Rojas <jr.rojas0327@gmail.com>
2025-12-01 13:29:16 +0100

    feat(contacto): eliminar dependencias de GSAP y simplificar la clase LandingContacto

--- src/app/features/landing/pages/contacto/contacto.ts
+++ src/app/features/landing/pages/contacto/contacto.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, ElementRef, AfterViewInit, inject } from '@angular/core';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 @Component({
   selector: 'landing-contacto',
@@ -8,37 +7,5 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
   templateUrl: './contacto.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingContacto implements AfterViewInit {
-  private el = inject(ElementRef);
-
-  ngAfterViewInit() {
-    const mapContainer = this.el.nativeElement.querySelector('.w-full.md\\:w-6\\/12.flex.justify-center');
-    const textContainer = this.el.nativeElement.querySelector('.w-full.md\\:w-6\\/12.text-sm');
-
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: this.el.nativeElement,
-        start: 'top 80%',
-        toggleActions: 'play none none reverse'
-      }
-    });
-
-    if (mapContainer) {
-      tl.from(mapContainer, {
-        x: -50,
-        opacity: 0,
-        duration: 0.8,
-        ease: 'power3.out'
-      });
-    }
-
-    if (textContainer) {
-      tl.from(textContainer, {
-        x: 50,
-        opacity: 0,
-        duration: 0.8,
-        ease: 'power3.out'
-      }, mapContainer ? '-=0.6' : '0');
-    }
-  }
+export class LandingContacto {
 }
